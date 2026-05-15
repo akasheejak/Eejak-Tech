b67258b3fd7e330145c9b0a78f047076b87567e5
Aakash Patel <deepak@eejaktech.com>
2026-05-15 11:19:59 +0530

    Talwind is working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center font-bold bg-black text-white text-4xl">
+      Eejak App
+    </div>
+  );
+};
+
+export default App;
