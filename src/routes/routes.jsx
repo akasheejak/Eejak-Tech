97d864714ebda2a07b4a6a1e3589612c65379b08
Aakash Patel <deepak@eejaktech.com>
2026-05-19 17:15:09 +0530

    creating the routes of the cyber page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,61 +1,60 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import Home from '../page/Home';
-import About from '../page/About';
-import Service from '../page/Service';
-import Contact from '../page/Contact';
-import WebDevelopment from '../page/WebDevelopment';
-import MobileDevelopment from '../page/MobileDevelopment';
-import SEOServices from '../page/SEOServices';
-import DevOps from '../page/DevOps';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../page/Home";
+import About from "../page/About";
+import Service from "../page/Service";
+import Contact from "../page/Contact";
+import WebDevelopment from "../page/WebDevelopment";
+import MobileDevelopment from "../page/MobileDevelopment";
+import SEOServices from "../page/SEOServices";
+import DevOps from "../page/DevOps";
 
-import AiMl from '../page/AiMl';
-import CyberSecurity from '../page/CyberSecurity';
-
+import AiMl from "../page/AiMl";
+import CyberSecurity from "../page/CyberSecurity";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/about',
+        path: "/about",
         element: <About />,
       },
       {
-        path: '/services',
+        path: "/services",
         element: <Service />,
       },
       {
-        path: '/contact',
+        path: "/contact",
         element: <Contact />,
       },
       {
-        path: '/services/web-development',
+        path: "/services/web-development",
         element: <WebDevelopment />,
       },
       {
-        path: '/services/mobile-development',
+        path: "/services/mobile-development",
         element: <MobileDevelopment />,
       },
       {
-        path: '/services/seo-services',
+        path: "/services/seo-services",
         element: <SEOServices />,
       },
       {
-        path: '/services/devops',
+        path: "/services/devops",
         element: <DevOps />,
       },
       {
-        path: '/services/ai-ml',
+        path: "/services/ai-ml",
         element: <AiMl />,
       },
       {
-        path: '/services/cyber-security',
+        path: "/services/cyber-security",
         element: <CyberSecurity />,
       },
     ],
